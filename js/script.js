e580d3291c4b519b8d1e5d4f75c3bdf7eb0b7e5f
marcelino05052000 <marcelino05052000@gmail.com>
2018-05-25 16:45:27 -0300

    Adicionado boneco teste e movimentação

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
-/*(function(){
-    //console.log(Phaser);
-    var game = new Phaser.Game(800,600,Phaser.AUTO,null,{preload:preload,create:create,update:update});
-
-    //carrega recursos
-    function preload(){
-        game.load.image('sky','img/sky.png');
-    }
-
-    //criação dos elementos do jogo
-    function create(){
-        game.add.sprite(0,0,'sky');
-    }
-
-    //estabelecer regras do jogo, lógica
-    function update(){}
-}()); */
-
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -27,6 +16,9 @@ var config = {
     }
 };
 
+var player;
+var platforms;
+var cursors;
 var game = new Phaser.Game(config);
 
 function preload ()
@@ -37,32 +29,97 @@ function preload ()
     this.load.image('altar','img/altar.png');
     this.load.image('espada','img/espada.png');
     this.load.image('arbusto','img/arbusto.png');
+    this.load.spritesheet('boneco','img/boneco.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create ()
 {
-    this.add.sprite(50,550,'chao');
+    this.add.sprite(400,252,'ceu');
+
+    /*this.add.sprite(50,550,'chao');
     this.add.sprite(150,550,'chao');
     this.add.sprite(250,550,'chao');
     this.add.sprite(350,550,'chao');
     this.add.sprite(450,550,'chao');
     this.add.sprite(550,550,'chao');
     this.add.sprite(650,550,'chao');
-    this.add.sprite(750,550,'chao');
+    this.add.sprite(750,550,'chao');*/
 
-    this.add.sprite(400,252,'ceu');
+    platforms = this.physics.add.staticGroup();
 
-    this.add.sprite(140,354,'arvore').setScale(2);
-    this.add.sprite(660,354,'arvore').setScale(2);
+    platforms.create(50,550,'chao').refreshBody();
+    platforms.create(150,550,'chao');
+    platforms.create(250,550,'chao');
+    platforms.create(350,550,'chao');
+    platforms.create(450,550,'chao');
+    platforms.create(550,550,'chao');
+    platforms.create(650,550,'chao');
+    platforms.create(750,550,'chao');
 
-    this.add.sprite(400,430,'altar');
+    this.add.sprite(140,354,'arvore');
+    this.add.sprite(660,354,'arvore');
+
+    platforms.create(400,430,'altar').refreshBody();
 
     this.add.sprite(404,360,'espada');
 
     this.add.sprite(60,480,'arbusto');
     this.add.sprite(730,480,'arbusto');
+
+    player = this.physics.add.sprite(100, 450, 'boneco').setScale(2);
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('boneco', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'boneco', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('boneco', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(player, platforms);
+   
 }
 
 function update ()
 {
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 }
